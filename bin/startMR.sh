d8ecbb6011c9ea8f76213d513d85bd77db7b8fa7
fuxiaoming <fuxiaoming@kingsoft.com>
2023-08-20 22:59:11 +0800

    add : 新增日志目录

--- bin/startMR.sh
+++ bin/startMR.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
-#一键启动脚本
+# 一键启动脚本
 
-#mymr-1.0.jar在target目录下
+# mymr-1.0.jar在target目录下
 JAR_PATH="../target/mymr-1.0.jar"
 
+# 创建log目录
+LOG_DIR="./log"
+mkdir -p "$LOG_DIR"
+
 # 启动 WordCountDriver 的主函数
-java -cp "$JAR_PATH" com.ksc.wordcount.driver.WordCountDriver &
+java -cp "$JAR_PATH" com.ksc.wordcount.driver.WordCountDriver > "$LOG_DIR/WordCountDriver.log" 2>&1 &
+
+# 休眠2秒
+sleep 2
 
 # 启动 Executor 的主函数
-java -cp "$JAR_PATH" com.ksc.wordcount.worker.Executor &
+java -cp "$JAR_PATH" com.ksc.wordcount.worker.Executor > "$LOG_DIR/Executor.log" 2>&1 &
 
